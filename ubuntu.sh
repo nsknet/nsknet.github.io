225e18f7d15586c52747f9a65d49f64938b08987
NSK <nsknet@users.noreply.github.com>
2025-03-25 15:05:02 +0700

    mongodb 8

--- ubuntu.sh
+++ ubuntu.sh
@@ -214,10 +214,10 @@ function install_mongodb(){
 	#https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
 	echo "========================================================================="
 	apt install  -y  gnupg curl
-	curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-   		sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   		--dearmor
-	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+	curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+	   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+	   --dearmor
+	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 
 	apt update
 	apt install -y mongodb-org
